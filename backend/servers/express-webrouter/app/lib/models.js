f1cba8e6ff7ed95aba39d275004ee4d59fe622ec
erwanledoux <lawanledoux@gmail.com>
2017-01-26 20:12:03 +0100

    fixed the mongo connection

--- backend/servers/express-webrouter/app/lib/models.js
+++ backend/servers/express-webrouter/app/lib/models.js
@@ -5,15 +5,25 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.Deputes = exports.mongooseConnection = undefined;
 
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
 var _mongoose = require('mongoose');
 
 var _mongoose2 = _interopRequireDefault(_mongoose);
 
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var secret = JSON.parse(_fs2.default.readFileSync(_path2.default.join(__dirname, '../../config/secret.json')));
 
 var mongoUrl = "mongodb://ec2-52-211-76-129.eu-west-1.compute.amazonaws.com:27017/wax_dfg";
 
 _mongoose2.default.Promise = global.Promise;
-var mongooseConnection = exports.mongooseConnection = _mongoose2.default.createConnection(mongoUrl);
+var mongooseConnection = exports.mongooseConnection = _mongoose2.default.createConnection(secret.MONGO_URL);
 
 var Deputes = exports.Deputes = mongooseConnection.model('', {}, 'deputes');
--- backend/servers/express-webrouter/app/src/models.js
+++ backend/servers/express-webrouter/app/src/models.js
@@ -1,8 +1,12 @@
+import fs from 'fs'
 import mongoose from 'mongoose'
+import path from 'path'
+
+const secret = JSON.parse(fs.readFileSync(path.join(__dirname, '../../config/secret.json')))
 
 const mongoUrl = "mongodb://ec2-52-211-76-129.eu-west-1.compute.amazonaws.com:27017/wax_dfg"
 
 mongoose.Promise = global.Promise
-export const mongooseConnection = mongoose.createConnection(mongoUrl)
+export const mongooseConnection = mongoose.createConnection(secret.MONGO_URL)
 
 export const Deputes = mongooseConnection.model('', {}, 'deputes')
