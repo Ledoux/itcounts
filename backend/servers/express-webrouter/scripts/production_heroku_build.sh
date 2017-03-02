9ff35b5cb0b56440578d11fa18f831b8b503f95e
erwanledoux <lawanledoux@gmail.com>
2017-03-02 13:22:52 +0100

    push to heroku

--- backend/servers/express-webrouter/scripts/production_heroku_build.sh
+++ backend/servers/express-webrouter/scripts/production_heroku_build.sh
@@ -1,15 +1,16 @@
-if [[ ! $(heroku apps:info -a toster) ]]; then
+if [[ ! $(heroku apps:info -a parite-au-pouvoir) ]]; then
   git init
-  heroku create --app itcounts --buildpack heroku/nodejs --remote production
-  heroku config:set --app itcounts TYPE=production
+  heroku create --app parite-au-pouvoir --buildpack heroku/nodejs --remote production
+  heroku config:set --app parite-au-pouvoir TYPE=production
 else
   echo "toster has been already created"
   echo "Do you wish to delete this app (y/n)?"
   read answer
   if echo "$answer" | grep -iq "^y" ;then
-    heroku apps:destroy itcounts --confirm itcounts;
-    heroku create --app itcounts --buildpack heroku/nodejs --remote production;
-    heroku config:set --app itcounts TYPE=production;
+    heroku apps:destroy parite-au-pouvoir --confirm parite-au-pouvoir;
+    heroku create --app parite-au-pouvoir --buildpack heroku/nodejs --remote production;
+    heroku config:set --app parite-au-pouvoir TYPE=production;
+    # heroku domains:add www.parite-au-pouvoir.org
   else
     echo No
   fi
--- backend/servers/express-webrouter/scripts/staging_heroku_build.sh
+++ backend/servers/express-webrouter/scripts/staging_heroku_build.sh
@@ -1,15 +1,16 @@
-if [[ ! $(heroku apps:info -a staging-itcounts) ]]; then
+if [[ ! $(heroku apps:info -a staging-parite-au-pouvoir) ]]; then
   git init
-  heroku create --app staging-itcounts --buildpack heroku/nodejs --remote staging
-  heroku config:set --app staging-itcounts TYPE=staging
+  heroku create --app staging-parite-au-pouvoir --buildpack heroku/nodejs --remote staging
+  heroku config:set --app staging-parite-au-pouvoir TYPE=staging
 else
-  echo "staging-itcounts has been already created"
+  echo "staging-parite-au-pouvoir has been already created"
   echo "Do you wish to delete this app (y/n)?"
   read answer
   if echo "$answer" | grep -iq "^y" ;then
-    heroku apps:destroy staging-itcounts --confirm staging-itcounts;
-    heroku create --app staging-itcounts --buildpack heroku/nodejs --remote staging;
-    heroku config:set --app staging-itcounts TYPE=staging;
+    heroku apps:destroy staging-parite-au-pouvoir --confirm staging-parite-au-pouvoir;
+    heroku create --app staging-parite-au-pouvoir --buildpack heroku/nodejs --remote staging;
+    heroku config:set --app staging-parite-au-pouvoir TYPE=staging;
+    # heroku domains:add www.staging-parite-au-pouvoir.org
   else
     echo No
   fi
