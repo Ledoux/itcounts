56e4574ac578a502398191521a14bc5984fcbb58
erwanledoux <lawanledoux@gmail.com>
2017-03-28 16:31:02 +0200

    added domain for production

--- backend/servers/express-webrouter/scripts/production_heroku_build.sh
+++ backend/servers/express-webrouter/scripts/production_heroku_build.sh
@@ -2,6 +2,7 @@ if [[ ! $(heroku apps:info -a pariteaupouvoir) ]]; then
   git init
   heroku create --app pariteaupouvoir --buildpack heroku/nodejs --remote production
   heroku config:set --app pariteaupouvoir TYPE=production
+  heroku domains:add --app pariteaupouvoir www.pariteaupouvoir.com
   if [[ ! -d "./scripts/production_secret.sh" ]]; then
     source ./scripts/production_secret.sh
   fi
