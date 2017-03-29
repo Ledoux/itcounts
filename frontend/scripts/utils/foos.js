0d8dfa50ca544f33c4a86fb8d0b1b0120ebf116f
erwanledoux <lawanledoux@gmail.com>
2017-03-29 13:55:43 +0200

    replaced VEGA par PAP

--- frontend/scripts/utils/foos.js
+++ frontend/scripts/utils/foos.js
@@ -1,4 +1,4 @@
 export const FACEBOOK_APP_ID = 906839432685871
 export const PROD_URL = 'https://pariteaupouvoir.herokuapp.com'
 export const SWEET_PROD_URL = 'http://www.pariteaupouvoir.com'
-export const HASHTAGS = '#VEGA #Parité #Politique'
+export const HASHTAGS = '#PAP #Parité #Politique'
